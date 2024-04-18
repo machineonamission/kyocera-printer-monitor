1d40526033ad4695d9212e9b61ba932b6690b8c3
Melody <reticivis@gmail.com>
2024-04-17 21:24:20 -0500

    workin gon this

--- src/js.rs
+++ src/js.rs
@@ -1,15 +1,32 @@
 use std::sync::Arc;
 
 use anyhow::{anyhow, Result};
-use deno_core::{JsRuntime, RuntimeOptions};
 use serde_json::{Map, Value};
 use tokio::sync::Mutex;
 
 pub(crate) type Object = Map<String, Value>;
 
-pub fn init() -> JsRuntime {
-    // init js runtime
-    JsRuntime::new(RuntimeOptions::default())
+pub struct JsRuntime {
+    platform: v8::SharedRef<v8::Platform>,
+    //TODO
+}
+
+pub fn init() {
+    let platform = v8::new_default_platform(0, false).make_shared();
+    v8::V8::initialize_platform(platform);
+    v8::V8::initialize();
+
+    // Create a new Isolate and make it the current one.
+    let isolate = &mut v8::Isolate::new(v8::CreateParams::default());
+
+    // Create a stack-allocated handle scope.
+    let handle_scope = &mut v8::HandleScope::new(isolate);
+
+    // Create a new context.
+    let context = v8::Context::new(handle_scope);
+
+    // Enter the context for compiling and running the hello world script.
+    v8::ContextScope::new(handle_scope, context)
 }
 
 pub fn cursed_js_to_object(runtime: &mut JsRuntime, script: String) -> Result<Object> {
